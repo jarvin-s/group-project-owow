14cb1807ece18d7ad7d70bcf5da5e430e08f5800
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-12-07 17:06:39 +0100

    refactor: clean up tracker file, remove unused admin page

--- src/components/tracker.tsx
+++ src/components/tracker.tsx
@@ -6,7 +6,6 @@ import { Pixelify_Sans } from "next/font/google";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import {
-  getDailyCalories,
   updateDailyCalories,
   incrementGoalCompletions,
   getUserProgress,
